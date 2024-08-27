e93c0968cf4744291c6b6cccdd8b0cb53f466b6c
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-27 09:34:51 +0900

    Create 20240826JSaplusb.js

---
+++ JavaScript, jQuery/20240826JSaplusb.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <p id="name"> </p>
+    <script>
+        var a = 100;
+        var b = 200;
+        document.getElementById('name').innerHTML= a + b;
+    </script>
+</body>
+</html>
