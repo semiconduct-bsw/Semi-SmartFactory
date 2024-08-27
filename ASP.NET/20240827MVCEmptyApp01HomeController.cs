743f821858ed7a1009e97f9c5ec7c1a8428c92da
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-27 11:21:07 +0900

    Update 20240827MVCEmptyApp01HomeController.cs

--- ASP.NET/20240827MVCEmptyApp01HomeController.cs
+++ ASP.NET/20240827MVCEmptyApp01HomeController.cs
@@ -14,3 +14,29 @@ namespace _20240827_MVCEmptyApp01.Controllers
         }
     }
 }
+
+/////////////////////////////////////////////////////////////
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    public class HelpController : ControllerBase
+    {
+        // /Help 또는 /Help/{id} 경로로 요청을 처리한다.
+        [Route("/Help/{id?}")]
+        public int Help(int? id)
+        {
+            if (id.HasValue)
+            {
+                // id가 전달된 경우, 해당 ID를 반환한다.
+                return id.Value;
+            }
+            else
+            {
+                // id가 전달되지 않은 경우, 기본값으로 0을 반환한다.
+                return 0;
+            }
+        }
+    }
+}
