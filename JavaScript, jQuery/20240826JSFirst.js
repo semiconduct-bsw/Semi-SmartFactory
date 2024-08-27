857d1ea582c120e5721aa3c4b8f93ed6bc87ad6c
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-27 09:33:47 +0900

    Create 20240826JSFirst.js

---
+++ JavaScript, jQuery/20240826JSFirst.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        /* alert = 팝업 창 */
+        /* 서버를 들어갈 때 팝업되는 형태 */
+        alert('Hello World!');
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
+
+/* --------------------------------------------- */
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <p id="name"> </p>
+    <script>
+        /* 아이디를 부여하여 브라우저에 기입 */
+        document.getElementById('name').innerHTML='Hello World';
+    </script>
+</body>
+</html>
