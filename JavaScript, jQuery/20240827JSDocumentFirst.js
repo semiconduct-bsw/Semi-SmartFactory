940a6f7fcbd2be29287ddc212cbab455fbd6b985
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-27 09:35:39 +0900

    Create 20240827JSDocumentFirst.js

---
+++ JavaScript, jQuery/20240827JSDocumentFirst.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        let pi = 3.141592;
+        let value1 = 7;
+
+        let result = pi * value1;
+        alert(result);
+    </script>
+</head>
+<body>
+    <p id="ppp"> </p>
+</body>
+</html>
