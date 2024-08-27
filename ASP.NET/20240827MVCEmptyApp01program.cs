5f62308947d6acd91740d0e5cf7a63d4f3b690d7
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-27 10:29:13 +0900

    Update 20240827MVCEmptyApp01program.cs

--- ASP.NET/20240827MVCEmptyApp01program.cs
+++ ASP.NET/20240827MVCEmptyApp01program.cs
@@ -5,11 +5,19 @@ namespace _20240827_MVCEmptyApp01
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            // MVC로 넘어가는 중간단계 처리
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
-            app.MapGet("/greet", () => "<h1>안녕하세요!</h1>");
+            //app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/greet", () => "<h1>안녕하세요!</h1>");
+            //app.MapGet("/help", () => "도와주세요~");
 
+            // 매핑
+            app.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                );
             app.Run();
         }
     }
