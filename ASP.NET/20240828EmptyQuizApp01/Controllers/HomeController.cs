6e8ad811db1654b2ee5612285c8806b0e91d957f
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-28 13:30:28 +0900

    Add files via upload

---
+++ ASP.NET/20240828EmptyQuizApp01/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _20240828_EmptyQuizApp01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyPage()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult InputQuiz()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult OutputQuiz(int number)
+        {
+            // 받은 number를 다시 넘겨주는 역할
+            ViewBag.Result = number;
+            return View();
+        }
+    }
+}
