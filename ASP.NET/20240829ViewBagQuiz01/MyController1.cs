aaeeb5073d012a9435d891c34d279dd80b69035a
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-29 10:24:39 +0900

    Add files via upload

---
+++ ASP.NET/20240829ViewBagQuiz01/MyController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _20240829_ViewBagQuiz01.Controllers
+{
+    public class MyController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Output()
+        {
+            ViewBag.data1 = new List<string>()
+            {
+                "C#프로그래밍", "Java 정복", "HTML5", "CSS 하루만에하기"
+            };
+
+            string[] foods = { "된장국", "김치찌개", "소금빵", "두루치기" };
+            TempData["temp01"] = foods;
+            return View();
+        }
+    }
+}
