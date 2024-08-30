308819c5ab82077f4f9d7a36edddf94a12a40df4
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-30 11:19:06 +0900

    Add files via upload

---
+++ ASP.NET/20240830CalculateQuizApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _20240830_CalculateQuizApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int number1, int number2)
+        {
+            ViewBag.Result1 = number1 + number2;
+            ViewBag.Result2 = number1 - number2;
+            ViewBag.Result3 = number1 * number2;
+            ViewBag.Result4 = number2 != 0 ? (double)number1 / (double)number2 : double.NaN;
+
+            return View();
+        }
+    }
+}
