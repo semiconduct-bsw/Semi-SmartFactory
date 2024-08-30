81d9cadf7baa4065287b6b8b20a8ad607875d7e4
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-08-30 11:19:55 +0900

    Add files via upload

---
+++ ASP.NET/20240830PIBWweightApp/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(double height, double weight, string gender)
+        {
+            // 키를 cm에서 m로 변환
+            double heightInMeters = height / 100.0;
+
+            // 성별에 따른 표준 체중 계산
+            double standardWeight;
+            if (gender == "남성")
+            {
+                standardWeight = heightInMeters * heightInMeters * 22;
+            }
+            else // 여성
+            {
+                standardWeight = heightInMeters * heightInMeters * 21;
+            }
+
+            // 표준 체중 대비 백분율(PIBW) 계산
+            double pibw = (weight / standardWeight) * 100;
+            string result;
+
+            // PIBW 값에 따른 체중 구분
+            if (pibw < 90)
+            {
+                result = "저체중";
+            }
+            else if (pibw >= 90 && pibw < 110)
+            {
+                result = "정상 체중";
+            }
+            else if (pibw >= 110 && pibw < 120)
+            {
+                result = "과체중";
+            }
+            else if (pibw >= 120 && pibw < 130)
+            {
+                result = "경도 비만";
+            }
+            else if (pibw >= 130 && pibw < 160)
+            {
+                result = "중정도 비만";
+            }
+            else
+            {
+                result = "고도 비만";
+            }
+
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/20240830PIBWweightApp/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// MVC 서비스 추가
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
