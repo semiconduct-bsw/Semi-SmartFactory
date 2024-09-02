6dbcf3cf3ac4b710610204d2c796acbfa675ee09
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-09-02 17:26:34 +0900

    Add files via upload

---
+++ ASP.NET/20240902ValidationAttribute01/HomeController.cs
@@ -0,0 +1,38 @@
+using _20240902_ValidationAttribute.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _20240902_ValidationAttribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+            return View(std);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET/20240902ValidationAttribute01/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _20240902_ValidationAttribute.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "이름을 작성해 주세요")]
+        [StringLength(15, MinimumLength = 2, 
+            ErrorMessage ="이름은 최소 2자 이상 15자 이하로 작성해주세요")]
+        public string Name { get; set; }
+
+        // ? = Null 일 수 있다는 의미
+        [Required(ErrorMessage = "나이를 작성해 주세요")]
+        [Range(20, 75,
+            ErrorMessage = "20세~75세로 작성해주세요")]
+        public int? Age { get; set; }
+        public string Password { get; set; }
+    }
+}
