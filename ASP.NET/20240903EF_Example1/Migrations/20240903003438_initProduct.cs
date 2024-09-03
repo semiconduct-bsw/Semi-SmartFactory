d84fe4ca811caa88250d70025f554f66bc476038
semiconduct-bsw <chem1bsw.fac@gmail.com>
2024-09-03 14:57:43 +0900

    Add files via upload

---
+++ ASP.NET/20240903EF_Example1/Migrations/20240903003438_initProduct.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _20240903_EF_Example1.Migrations
+{
+    /// <inheritdoc />
+    public partial class initProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Zone = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.No);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ ASP.NET/20240903EF_Example1/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _20240903_EF_Example1
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int No { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Zone { get; set; }
+    }
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (local)\\SQLEXPRESS; " +
+                        "Database = AndongDb; " +
+                        "Trusted_Connection = True;" +
+                        "Encrypt=False");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductDbContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+            //Console.WriteLine("데이터베이스 테이블이 생성되었습니다.");
+
+            // 데이터 삽입 (No 자동삽입 in MSSQL)
+            //var product = new Product()
+            //{
+            //    Name = "3분카레",
+            //    Zone = "경북 상주시"
+            //};
+            //context.Product.Add(product);
+            //context.SaveChanges();
+            //Console.WriteLine("데이터 삽입 성공");
+
+            // 데이터 조회
+            var list= context.Product.ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine($"번호 : {item.No}, 제품명 : {item.Name}, 지역 : {item.Zone}");
+            }
+
+            context.Dispose();
+        }
+    }
+}
