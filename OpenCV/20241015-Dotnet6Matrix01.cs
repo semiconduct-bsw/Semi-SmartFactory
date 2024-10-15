68ace27c754553dcc5c0b7f04f551397fdbdc86c
배선우 <chem1bsw.fac@gmail.com>
2024-10-15 13:59:54 +0900

    Update 20241015-Dotnet6Matrix01.cs

--- OpenCV/20241015-Dotnet6Matrix01.cs
+++ OpenCV/20241015-Dotnet6Matrix01.cs
@@ -6,10 +6,10 @@ namespace _20241015_dotnet6
     {
         static void Main(string[] args)
         {
-            float[] data =
+            double[] data =
             {
-                1.2f, 2.3f, 3.2f,
-                4.5f, 5.0f, 6.5f
+                1.2, 2.3, 3.2,
+                4.5, 5.0, 6.5
             };
 
             Mat m1 = new Mat(2, 3, MatType.CV_8U);
@@ -17,12 +17,16 @@ namespace _20241015_dotnet6
             Mat m2 = new Mat(2, 3, MatType.CV_8U, new Scalar(255));
             Mat m3 = new Mat(2, 3, MatType.CV_16S, new Scalar(300));
 
+            Size sz = new Size(3, 2);
+            Mat m5 = new Mat(sz, MatType.CV_64F, new Scalar(0));
+
             Console.WriteLine("m1 : \n" + m1.Dump());
             Console.WriteLine("m2 : \n" + m2.Dump());
             Console.WriteLine("m3 : \n" + m3.Dump());
+            Console.WriteLine("m5 : \n" + m5.Dump());
 
             // 메모리에서 명시적으로 지워주기 위한 코드
-            m1.Dispose();
+            m1.Dispose(); m2.Dispose(); m3.Dispose(); m5.Dispose();
         }
     }
 }
