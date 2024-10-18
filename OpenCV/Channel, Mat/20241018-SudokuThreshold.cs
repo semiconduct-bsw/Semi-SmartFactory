c41b930b2e82f677b7cdfa5fe3f114e1b77c357c
배선우 <chem1bsw.fac@gmail.com>
2024-10-18 14:22:06 +0900

    Create 20241018-SudokuThreshold.cs

---
+++ OpenCV/Channel, Mat/20241018-SudokuThreshold.cs
@@ -0,0 +1,36 @@
+using OpenCvSharp;
+
+namespace _20241018_SudokuThreshold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 이미지 파일 경로
+            string imagePath = @"c:/temp/opencv/sudoku.png";
+
+            // 이미지를 그레이스케일로 읽기
+            Mat src = Cv2.ImRead(imagePath, ImreadModes.Grayscale);
+            Cv2.ImShow("src", src);
+
+            // Otsu의 이진화 적용, 어려우니 일단 받아들이기
+            Mat dst = new Mat();
+            Cv2.Threshold(src, dst, 0, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);
+            Cv2.ImShow("dst", dst);
+
+            // 적응형 이진화 (Mean C)
+            Mat dst2 = new Mat();
+            Cv2.AdaptiveThreshold(src, dst2, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.Binary, 51, 7);
+            Cv2.ImShow("dst2", dst2);
+
+            // 적응형 이진화 (Gaussian C)
+            Mat dst3 = new Mat();
+            Cv2.AdaptiveThreshold(src, dst3, 255, AdaptiveThresholdTypes.GaussianC, ThresholdTypes.Binary, 51, 7);
+            Cv2.ImShow("dst3", dst3);
+
+            // 대기 및 창 닫기
+            Cv2.WaitKey();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
