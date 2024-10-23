b764a9e3f8e3697c19d11e5e5365379a8d99c396
배선우 <chem1bsw.fac@gmail.com>
2024-10-23 16:36:58 +0900

    Create 20241023-NumpyPrac.py

---
+++ PyTorch/Basic/20241023-NumpyPrac.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# 1차원 배열
+vec = np.array([1, 2, 3, 4, 5])
+print(vec)
+
+# 2차원 배열
+mat = np.array([[10, 20, 30], [ 60, 70, 80]]) 
+print(mat)
+
+print('vec의 타입 :',type(vec))
+print('mat의 타입 :',type(mat))
