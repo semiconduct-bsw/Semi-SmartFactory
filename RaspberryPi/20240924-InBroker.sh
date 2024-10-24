2131976e36cc0d5cfbc81f3ae614840e7dd87aad
배선우 <chem1bsw.fac@gmail.com>
2024-10-24 17:30:50 +0900

    Create 20240924-InBroker.sh

---
+++ RaspberryPi/20240924-InBroker.sh
@@ -0,0 +1,3 @@
+/etc/mosquitto $ sudo mosquitto_passwd -b passwordfile mqtt_phirippa 1234
+pi@raspberrypi:/etc/mosquitto $ cat passwordfile
+pi@raspberrypi:/etc/mosquitto/conf.d $ sudo systemctl restart mosquitto
