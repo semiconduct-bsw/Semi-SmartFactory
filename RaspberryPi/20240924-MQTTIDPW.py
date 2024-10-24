7c7daf4b87ddcddfadf40ac6ba041cfa17b424c0
배선우 <chem1bsw.fac@gmail.com>
2024-10-24 17:29:16 +0900

    Create 20240924-MQTTIDPW.py

---
+++ RaspberryPi/20240924-MQTTIDPW.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def on_connect( client, userdata, flags, reason_code, properties ):
+   print(f“Connect with result code:{reason_code}“)
+   client.subscribe(“temp”)
+
+def on_message( client, userdata, msg ):
+   print( msg.topic +” “+str(msg.payload) )
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.username_pw_set(username=“mqtt_boy”, password=“1234”)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect(“localhost”, 1883, 60)
+mqttc.loop_forever( )
