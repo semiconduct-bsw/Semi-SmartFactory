18a517de23328c801d4921b6ce105e6af1cf9e88
배선우 <chem1bsw.fac@gmail.com>
2024-10-24 17:13:03 +0900

    Create I2C-OLEDHelloWorld.ino

---
+++ Arduino/Sensor/I2C-OLEDHelloWorld.ino
@@ -0,0 +1,19 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+void setup() {
+   Serial.begin(9600);
+   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+   }
+   display.clearDisplay();
+   display.setTextColor(WHITE);
+   display.println("Hello World!");
+   display.display();
+}
+void loop() { }
