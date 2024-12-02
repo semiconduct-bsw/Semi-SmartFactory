c5ef6aa7e41c7a8d4c66233643d1051322a78db0
배선우 <chem1bsw.fac@gmail.com>
2024-12-02 23:37:26 +0900

    Update FirstReportStructs.cs

--- 0.[최종프로젝트] SECS GEM Protocol Proj./2주차 - 통신구현,GUI,로그남기기/FirstReportStructs.cs
+++ 0.[최종프로젝트] SECS GEM Protocol Proj./2주차 - 통신구현,GUI,로그남기기/FirstReportStructs.cs
@@ -1,40 +1,105 @@
-namespace _20241112_CIMPCGUI01
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241112_CIMPCGUI01.Reports
 {
-    public struct IDReport
+    internal class ReportStructs
     {
-        public string MODELID;
-        public string OPID;
-        public string MATERIALID;
-    }
+        public struct OnlineRemoteChangedReport
+        {
+            public string StartType;  // 패킷 시작 문자
+            public string Code;       // "10701" 등 리포트 식별할 고유 코드
+            public string ReportID; // "IDReport" 등의 리포트 타입 정보
+            public string ModelID;
+            public string OPID;
+            public string MaterialID;
+            public string EndType;    // 패킷 끝 문자
+        }
 
-    public struct StartedReport
-    {
-        public string MODELID;
-        public string OPID;
-        public string PROCID;
-        public string MATERIALID;
-        public string LOTID;
-    }
+        public struct IDReport
+        {
+            public string StartType;  // 패킷 시작 문자
+            public string Code;       // "10701" 등 리포트 식별할 고유 코드
+            public string ReportID; // "IDReport" 등의 리포트 타입 정보
+            public string ModelID;
+            public string OPID;
+            public string MaterialID;
+            public string EndType;    // 패킷 끝 문자
 
-    public struct CanceledReport
-    {
-        public string MODELID;
-        public string OPID;
-        public string PROCID;
-        public string MATERIALID;
-        public string LOTID;
-    }
+        }
 
-    public struct CompletedReport
-    {
-        public string MODELID;
-        public string OPID;
-        public string PROCID;
-        public string MATERIALID;
-        public string LOTID;
-        public string SENSOR0;
-        public string SENSOR1;
-        public string SENSOR2;
-        public string JUDGE;
+        public struct StartedReport
+        {
+            public string StartType;  // 패킷 시작 문자
+            public string Code;       // "10703" 등 리포트 식별할 고유 코드
+            public string ReportID; // 리포트 타입
+            public string ModelID;
+            public string OPID;
+            public string ProcID;
+            public string MaterialID;
+            public string LotID;
+            public string EndType;    // 패킷 끝 문자
+        }
+
+        public struct StartRcmd
+        {
+            public string StartType;
+            public string Code;
+            public string RCMD;
+            public string ModelID;
+            public string ProcID;
+            public string MaterialID;
+            public string LotID;
+            public string EndType;
+        }
+
+        public struct CanceledReport
+        {
+            public string StartType;  // 패킷 시작 문자
+            public string Code;       // "10704" 등 리포트 식별할 고유 코드
+            public string ReportID; // 리포트 타입
+            public string ModelID;
+            public string OPID;
+            public string ProcID;
+            public string MaterialID;
+            public string LotID;
+            public string EndType;    // 패킷 끝 문자
+        }
+
+        public struct CancleRcmd
+        {
+            public string StartType;
+            public string Code;
+            public string RCMD;
+            public string ModelID;
+            public string MaterialID;
+            public string CancleCode;
+            public string CancleReasonText;
+            public string EndType;
+        }
+
+        public struct CompletedReport
+        {
+            public string StartType;  // 패킷 시작 문자
+            public string Code;       // "10713" 등 리포트 식별할 고유 코드
+            public string ReportID;      // 리포트 타입
+            public string ModelID;
+            public string OPID;
+            public string ProcID;
+            public string MaterialID;
+            public string LotID;
+            public string Sen1;        // 센서 1 값 (전압)
+            public string Sen2;        // 센서 2 값 (전류)
+            public string Sen3;        // 센서 3 값 (열)
+            public string Jud1;
+            public string Jud2;
+            public string Jud3;
+            public string TotalJud;
+            public string EndType;    // 패킷 끝 문자
+        }
     }
 }
